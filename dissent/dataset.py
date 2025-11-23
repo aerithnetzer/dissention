bb056562aea53ed431540da91239b13f6dcfeee8
aerithnetzer <aerithnetzer@outlook.com>
2025-11-22 22:42:53 -0600

    Update dataset for better logging

--- dissent/dataset.py
+++ dissent/dataset.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-
 from loguru import logger
 import pandas as pd
 import typer
@@ -35,15 +34,19 @@ def main():
         RAW_DATA_DIR / "dockets-2024-12-31.csv.bz2", nrows=100_000, quotechar="`"
     )
     print(dockets.columns)
-
+    logger.info("Finished reading dataframes")
     # First join: opinions with opinion_clusters
     df = opinions.set_index("cluster_id").join(
         opinion_clusters.set_index("id"), how="inner", rsuffix="_cluster"
     )
 
+    logger.info("Finished first join")
     df = df.join(dockets.set_index("id"), on="docket_id", how="inner", rsuffix="_docket")
 
+    logger.info("Finished second join")
     df.to_csv(INTERIM_DATA_DIR / "dataset.csv.bz2", quotechar="`")
+
+    logger.info("Finished writing dataset")
 
 
 if __name__ == "__main__":
