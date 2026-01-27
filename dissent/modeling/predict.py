35a4e440de9ffd78312d508fcccbffdab6d0f643
aerithnetzer <aerithnetzer@outlook.com>
2026-01-27 12:35:21 -0600

    update predict

--- dissent/modeling/predict.py
+++ dissent/modeling/predict.py
@@ -9,7 +9,7 @@ logging.basicConfig(
     format="%(asctime)s : %(levelname)s : %(message)s",
     level=logging.INFO,
 )
-OUTPUT_DIR = MODELS_DIR / "iteration_0003"
+OUTPUT_DIR = MODELS_DIR / "iteration_0005"
 _ = os.makedirs(OUTPUT_DIR, exist_ok=True)
 WORKERS = 32
 app = typer.Typer()
@@ -20,20 +20,36 @@ def main():
     model_files = sorted(list(OUTPUT_DIR.glob("*.model")))
     model_path = str(model_files[-1])
     model = Word2Vec.load(model_path)
-    sims = model.wv.most_similar("dissent")
-    print(f"{sims}\n\n")
-    sims = model.wv.most_similar("concur")
-    print(f"{sims}\n\n")
-    sims = model.wv.most_similar("murder")
-    print(f"{sims}\n\n")
-    sims = model.wv.most_similar("constitution")
-    print(f"{sims}\n\n")
-    sims = model.wv.most_similar("constitution")
-    print(f"{sims}\n\n")
-    sims = model.wv.most_similar("precedent")
-    print(f"{sims}\n\n")
-    sims = model.wv.most_similar("precedent")
-    print(f"{sims}\n\n")
+
+    words_to_test = [
+        "undemocratic",
+        "concur",
+        "murder",
+        "constitution",
+        "precedent",
+        "abortion",
+        "gender",
+        "sex",
+        "education",
+        "religion",
+        "homosexual",
+        "woman",
+        "female",
+        "male",
+        "freedom",
+        "poverty",
+        "wealth",
+        "voting",
+        "trump",
+        "literalism",
+        "riot",
+        "insurrection",
+        "equality",
+        "strongly",
+    ]
+    for w in words_to_test:
+        sims = model.wv.most_similar(w)
+        print(f"\n\nWords most similar to {w}:\n", str(sims))
 
 
 if __name__ == "__main__":
